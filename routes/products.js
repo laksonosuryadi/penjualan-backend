bbc3cd0f7453337a628f5c08abd9ec756dd60035
laksonosuryadi <laksono.suryadi@gmail.com>
2017-05-23 20:13:36 +0700

    adding authentication in products routes

--- routes/products.js
+++ routes/products.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var product = require('../controllers/productController');
+var helper = require('../helper/authJWT');
 
 /* SHOW PRODUCTS */
 router.get('/', product.getAllProducts);
 
 /* ADD PRODUCT */
-router.post('/', product.addProduct);
+router.post('/', helper.verify, product.addProduct);
 
 /* DELETE PRODUCT */
-router.delete('/:productId', product.deleteProduct);
+router.delete('/:productId', helper.verify, product.deleteProduct);
 
 /* UPDATE PRODUCT */
-router.put('/:productId', product.updateProduct);
+router.put('/:productId', helper.verify, product.updateProduct);
 
 module.exports = router;
